f971d45349421d3d492ce569ce98bdd4f5b57a34
Michel Makei Gefuni <michelgefuni@gmail.com>
2020-07-21 15:56:17 -0300

    Added Isomorphic request and request for random joke

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import 'isomorphic-fetch';
 
-const Home = () => <h1>Hello World</h1>;
+const headers = new Headers();
+
+headers.append('x-rapidapi-host', 'jokeapi-v2.p.rapidapi.com');
+headers.append('x-rapidapi-key', '9249af1e56mshfbd3f87269e1899p167d7fjsnc3c9dde60d52');
+
+const fetchInit = {
+  method: 'GET',
+  headers: headers,
+};
+
+const Home = ({ joke }: any) => <h1>{joke.joke}</h1>;
+
+Home.getInitialProps = async () => {
+  const response = await fetch('https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&blacklistFlags=nsfw,racist&idRange=0-150', fetchInit);
+  const joke = await response.json();
+
+  return { joke }
+}
 
 export default Home;
