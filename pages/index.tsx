0124e68c43a66cf147347e6c593d74f03012d5c9
Michel Makei Gefuni <michelgefuni@gmail.com>
2020-07-21 17:53:01 -0300

    Fixed return when has a question and fixed env file

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,22 @@ const fetchInit = {
   headers: headers,
 };
 
-const Home = ({ joke }: any) => <h1>{joke.joke}</h1>;
+const Home = ({ joke }: any) => {
+  if (!joke.id) {
+    return <h1>Oops! Something gets wrong!</h1>;
+  }
+
+  return <>
+    {!!joke.category && <strong>{joke.category}</strong>}
+    <h1>{joke.joke || joke.setup}</h1>
+    {!!joke.delivery && <h2>{joke.delivery}</h2>}
+  </>
+
+}
 
 Home.getInitialProps = async () => {
   const response = await fetch('https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&blacklistFlags=nsfw,racist&idRange=0-150', fetchInit);
   const joke = await response.json();
-
   return { joke }
 }
 
