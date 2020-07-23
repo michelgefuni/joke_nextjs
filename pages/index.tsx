018c0223e78ff76398e4b0c33788eec21fac00aa
Michel Makei Gefuni <michelgefuni@gmail.com>
2020-07-23 18:10:42 -0300

    Added Vercel integration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,26 +1,6 @@
 import React from 'react';
 import 'isomorphic-fetch';
 
-const headers = new Headers();
-
-headers.append('x-rapidapi-host', 'jokeapi-v2.p.rapidapi.com');
-headers.append('x-rapidapi-key', process.env.REACT_APP_API_KEY || '');
-
-const fetchInit = {
-  method: 'GET',
-  headers: headers,
-};
-
-export const getStaticProps = async () => {
-  const response = await fetch('https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&blacklistFlags=nsfw,racist&idRange=0-150', fetchInit);
-  const joke = await response.json();
-  return {
-    props: {
-      joke
-    }
-  }
-}
-
 const Home = ({ joke }: any) => {
   if (!joke.id) {
     return <h1>Oops! Something gets wrong!</h1>;
@@ -31,6 +11,26 @@ const Home = ({ joke }: any) => {
     <h1>{joke.joke || joke.setup}</h1>
     {!!joke.delivery && <h2>{joke.delivery}</h2>}
   </>
+}
+
+export const getServerSideProps = async () => {
+  const headers = new Headers();
+  
+  headers.append('x-rapidapi-host', 'jokeapi-v2.p.rapidapi.com');
+  headers.append('x-rapidapi-key', process.env.NEXT_PUBLIC_ANALYTICS_ID || '');
+
+  const response = await fetch('https://jokeapi-v2.p.rapidapi.com/joke/Any?format=json&blacklistFlags=nsfw,racist&idRange=0-150', {
+    method: 'GET',
+    headers: headers,
+  });
+
+  const joke = await response.json();
+
+  return {
+    props: {
+      joke
+    }
+  }
 }
 
 export default Home;
